faa6e2c1828b2d92bfc5d4a483ff445a888ebcb6
Vagner Treguas <vagner.treguas@iv2.com.br>
2024-12-09 10:50:14 -0300

    feature: new rule to see wich user is logged

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LoginComponent } from '../../pages/login/login.component';
+import { LoginService } from '../../services/login.service';
 
 
 
@@ -13,5 +15,24 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  loginService = inject(LoginService);
+
+  userLogged = {
+    name: "",
+    role: ""
+  }
+
+  constructor() {
+    const user = JSON.parse(localStorage.getItem('userLogged') as string);
+    this.userLogged.name = user.name;
+    this.userLogged.role = user.role;
+  }
+
+  logout() {
+    this.loginService.logout();
+  }
+
+
+
 
 }
